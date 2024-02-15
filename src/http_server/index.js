a87087856535a24e0014498d6a5bd9c5e7220f9a
vasily <vasilymishanin@gmail.com>
2024-02-15 23:08:31 +0600

    init task websockets for battleship

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -3,15 +3,18 @@ import * as path from 'path';
 import * as http from 'http';
 
 export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
-        if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
-            return;
-        }
-        res.writeHead(200);
-        res.end(data);
-    });
+  const __dirname = path.resolve(path.dirname(''));
+  console.log(__dirname);
+  const file_path =
+    __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
+  console.log(file_path);
+  fs.readFile(file_path, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
 });
